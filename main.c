be4c9391942c29a6bd3cb239fde1c239f3a0ae0b
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-06-17 16:38:14 -0300

    feat(programmingC): adiciona comentário e uso de printf

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() { // ponto de entrada compilador C
+
+    // para lançar comentários de linha única
+
+    printf("Hello World\n"); // para projetar na tela
+
+    return 0; // função main retorna inteiro
+}
