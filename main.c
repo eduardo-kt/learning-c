dbcb25f068e285e07de524c6ba9f4f45d604f111
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-06-18 07:13:44 -0300

    feat(programmingC): demonstra format specifiers

--- main.c
+++ main.c
@@ -47,6 +47,18 @@ int main() { // ponto de entrada compilador C
         printf("Nao eh aluno\n");
     }
 
+    // format specifier
+    int n1 = 1;    
+    
+    printf("%4d\n", n1); // identa 4 casas para direita
+    printf("%05d\n", n1);  // identa 5 casas para direita preenchendo com zeros
+    printf("%-1d\n", n1); // identa 1 casa para esquerda
+    printf("%+05d\n", n1); // especifica que o sinal do número deve aparecer
+
+    float decimal1 = 14.078;
+
+    printf("%+10.2f", decimal1);
+    
 
 
 
