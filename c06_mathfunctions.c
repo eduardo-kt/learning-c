2c70a5ecf50e64f9129f7b5b9baf4ef156c7ed2c
Eduardo K Teixeira <eduardo.kunzel@hotmail.com>
2025-06-24 10:08:21 -0300

    feat(c06): exemplifica uso da biblioteca math

---
+++ c06_mathfunctions.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    // int x = 9;
+    
+    
+    
+
+    // x = sqrt(x);
+    // x = pow(x, 3);
+
+    // float x = 3.14;
+    // x = round(x);
+    // x = ceil(x);
+    // x = abs(x);
+    // x = log(x);
+
+    float x = 45;
+    // x = sin(x);
+    // x = cos(x);
+    // x = tan(x);
+
+
+    // printf("%d", x);
+    printf("%.2f", x);
+    
+
+    return 0;
+}
